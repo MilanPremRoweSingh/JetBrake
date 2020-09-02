78fd4ae56313fc308df69e03b3c781f8a3fdf9ba
Milan <milanprsingh@gmail.com>
2020-09-01 22:19:57 -0700

    Add next level loading logic

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class GameManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        DontDestroyOnLoad(this);
+    }
+
+    public void LoadNextScene()
+    {
+        int currSceneBuildIdx = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currSceneBuildIdx+1);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.N))
+        {
+            LoadNextScene();
+        }
+    }
+}
